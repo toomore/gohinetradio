5353066ae5dd625bda855e3b407a3e4a78fcc158
Toomore Chiang <toomore0929@gmail.com>
2015-07-08 19:17:27 +0800

    Rewrite for direct open radio.

--- gohinetradio/main.go
+++ gohinetradio/main.go
@@ -7,14 +7,16 @@ Install:
 
 Usage:
 
-	gohinetradio
+	gohinetradio [RadioNo]
 
 */
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"runtime"
@@ -23,26 +25,36 @@ import (
 	"github.com/toomore/gohinetradio"
 )
 
-func main() {
-	//PrintChannel()
-	gohinetradio.GenList()
-
-	//fmt.Println(GetRadioList(LISTPAGE))
-
-	fmt.Println("輸入廣播頻道編號：")
-	in := bufio.NewReader(os.Stdin)
-	stdString, _ := in.ReadString('\n')
-	radioNo := strings.Split(stdString, "\n")[0]
-	if len(radioNo) > 0 {
-		radioData, err := gohinetradio.GetURL(radioNo)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Printf("%s %s\n%s\n",
-				radioData.ChannelTitle, radioData.ProgramName, radioData.PlayRadio)
-			if runtime.GOOS == "darwin" {
-				exec.Command("/Applications/VLC.app/Contents/MacOS/VLC", radioData.PlayRadio).Start()
+func play(radioNo string) {
+	if radioData, err := gohinetradio.GetURL(radioNo); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s %s\n%s\n",
+			radioData.ChannelTitle, radioData.ProgramName, radioData.PlayRadio)
+		if runtime.GOOS == "darwin" {
+			if err := exec.Command("/Applications/VLC.app/Contents/MacOS/VLC", radioData.PlayRadio).Start(); err != nil {
+				log.Fatal(err)
 			}
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	var args = flag.Args()
+
+	if len(args) == 1 {
+		play(args[0])
+	} else {
+		//PrintChannel()
+		gohinetradio.GenList()
+		//fmt.Println(GetRadioList(LISTPAGE))
+		fmt.Println("輸入廣播頻道編號：")
+		in := bufio.NewReader(os.Stdin)
+		stdString, _ := in.ReadString('\n')
+		radioNo := strings.Split(stdString, "\n")[0]
+		if len(radioNo) > 0 {
+			play(radioNo)
 		}
 	}
 }
